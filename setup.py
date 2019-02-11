d1abf68aa25ab88507c2cc0883d60c10043f1273
Brian Huang <brian@hyperanna.com>
2019-02-11 11:47:00 +0000

    [Init] Structure project via setup.py and Makefile

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open('README.MD', encoding='UTF-8') as f:
+    readme = f.read()
+
+setup(
+    name='hr',
+    version='0.1.0',
+    description='manage users on a server based on an "inventory" JSON file',
+    long_description=readme,
+    author='Brian Huang',
+    author_email='bran_hhq@hotmail.com',
+    packages=find_packages('src'),
+    package_dir={'':'src'},
+    install_requires=[]
+)
