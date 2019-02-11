27b9aa365f645d554f2bafb3115caf3a5c00511e
Brian Huang <brian@hyperanna.com>
2019-02-11 12:48:27 +0000

    [TDD]First test failed the program

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,4 +1,14 @@
 import pytest
 
-def test_foo():
-    assert 2 == 2
+from hr import cli
+
+@pytest.fixture()
+def parser():
+    return cli.create_parser()
+
+def test_parser_without_driver():
+    """
+    Without a specified driver the parser will exit
+    """
+    with pytest.raises(SystemExit):
+        parser.parse_args()
